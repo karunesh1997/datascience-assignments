9dcf0d5dc40c3af7690551479fac5522a2c3857b
karunesh chaturvedi <57280336+karunesh1997@users.noreply.github.com>
2020-07-14 13:16:15 +0530

    Update Association_Rules (phone data).R

--- Association_Rules (phone data).R
+++ Association_Rules (phone data).R
@@ -51,38 +51,3 @@ inspect(rules_lift)
 ?plot
 
 plot(rules,method = "graph")
-
-
-########## Groceries Data Set #########
-library(arules)
-groceries<-read.transactions(file.choose(),format="basket")
-inspect(groceries[1:10])
-class(groceries)
-# itemFrequencyPlot can be applicable only for transaction data 
-# count of each item from all the transactions 
-itemFrequencyPlot(groceries,topN=20)
-groceries_rules<-apriori(groceries,parameter = list(support = 0.002,confidence = 0.05,minlen=3))
-
-library(arulesViz)
-plot(groceries_rules,method = "scatterplot")
-plot(groceries_rules,method = "grouped")
-plot(groceries_rules,method = "graph")
-plot(groceries_rules,method = "mosaic")
-
-
-### On inbuilt Data set #####
-library(arules)
-data("Groceries")
-summary(Groceries)
-inspect(Groceries[1:10])
-rules <- apriori(Groceries,parameter = list(support = 0.002,confidence = 0.05,minlen=5))
-inspect(rules[1:5])
-windows()
-plot(rules,method = "scatterplot")
-plot(rules,method = "grouped")
-plot(rules,method = "graph")
-plot(rules,method = "mosaic")
-
-rules <- sort(rules,by="lift")
-
-inspect(rules[1:4])
